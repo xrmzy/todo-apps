62df42adb78ad82dbfbd1d357064875d09647f97
xrmzy <ramdanahamd@gmail.com>
2022-09-29 17:08:02 +0700

    add makeTodo() and RENDER_EVENT

--- js/script.js
+++ js/script.js
@@ -52,5 +52,27 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const textTimestamp = document.createElement('p');
     textTimestamp.innerText = todoObject.timestamp;
+
+    const textContainer = document.createElement('div');
+    textContainer.classList.add('inner');
+    textContainer.append(textTitle, textTimestamp);
+
+    const container = document.createElement('div');
+    container.classList.add('item', 'shadow');
+    container.append(textContainer);
+    container.setAttribute('id', `todo-${todoObject.id}`);
+
+    return container;
   }
+
+  // event RENDER_EVENT
+  document.addEventListener(RENDER_EVENT, function () {
+    const uncompletedTODOList = document.getElementById('todo');
+    uncompletedTODOList.innerHTML = '';
+
+    for (const todoItem of todos) {
+      const todoElement = makeTodo(todoItem);
+      uncompletedTODOList.append(todoElement);
+    }
+  });
 });

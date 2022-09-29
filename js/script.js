c68fbb305b28cf0286299acde53704d540e55afa
xrmzy <ramdanahamd@gmail.com>
2022-09-29 15:10:48 +0700

    add show item Todo

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const submitForm = document.getElementById("form");
-  submitForm.addEventListener("submit", function (event) {
+document.addEventListener('DOMContentLoaded', function () {
+  const submitForm = document.getElementById('form');
+  submitForm.addEventListener('submit', function (event) {
     event.preventDefault();
     addTodo();
   });
 
+  const todos = [];
+  const RENDER_EVENT = 'event-todo';
   //function addTodo()
+
   function addTodo() {
-    const textTodo = document.getElementById("title").value;
-    const timestamp = document.getElementById("date").value;
+    const textTodo = document.getElementById('title').value;
+    const timestamp = document.getElementById('date').value;
 
     const genratedID = generatedId();
     const todoObject = generateTodoObject(
@@ -19,6 +22,35 @@ document.addEventListener("DOMContentLoaded", function () {
     );
     todos.push(todoObject);
 
-    document.dispatchEvent(new Event(REDNDER_EVENT));
+    document.dispatchEvent(new Event(RENDER_EVENT));
+  }
+
+  //function generatedId
+  function generatedId() {
+    return +new Date();
+  }
+
+  //function generatedTodoObject
+  function generateTodoObject(id, task, timestamp, isCompleted) {
+    return {
+      id,
+      task,
+      timestamp,
+      isCompleted,
+    };
+  }
+
+  //listener RENDER_EVENT using console.log to show array todos
+  document.addEventListener(RENDER_EVENT, function () {
+    console.log(todos);
+  });
+
+  //makeTodo() function to showing the datas in container
+  function makeTodo(todoObject) {
+    const textTitle = document.createElement('h2');
+    textTitle.innerText = todoObject.task;
+
+    const textTimestamp = document.createElement('p');
+    textTimestamp.innerText = todoObject.timestamp;
   }
 });
